cfb72b3d65ca5d928a049526dbd2498225771d4a
stephaneworkspace <s.bressani@bluewin.ch>
2019-11-25 20:46:53 +0100

    Begining of the project

--- configureserverdebian/useradd.py
+++ configureserverdebian/useradd.py
@@ -2,8 +2,28 @@
     My personal config for Ovh vps Debian
     Author: Stéphane Bressani <s.bressani@bluewin.ch>
 """
+from subprocess import Popen, CalledProcessError
 import crypt
 
-password = "p@ssw0rd"
-encPass = crypt.crypt(password, "22")
-"""useradd -p "+encPass+" johnsmith"""
+
+class useradd:
+    def __init__(self, username, password, log):
+        self.username = username
+        self.password = crypt.crypt(password, "22")
+        self.log = log
+
+    def cmd(self):
+        """
+        Command useradd
+        """
+        global USERADDOUTPUT
+        try:
+            cmd_list = ['useradd', '-p', self.password, self.useradd]
+            cmd = Popen(cmd_list)
+            cmd.wait
+            USERADDOUTPUT = cmd.communicate()[0]
+            self.log.add_log(USERADDOUTPUT)
+        except CalledProcessError:
+            # self.log.add_log(const.ERR_USERADD)
+            #
+            self.log.add_log('error msg')  # to do with user name
